8d2bae9cbfeee3d80737d1ffc446f1af27f1e9bb
Jai Sawhney <85261243+jaisawhney@users.noreply.github.com>
2023-05-08 22:16:25 -0700

    Forgot to add the solution...

--- smaller_numbers_than_current.py
+++ smaller_numbers_than_current.py
@@ -1,16 +1,27 @@
-"""
-Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        """
+        Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
 
-Input: nums = [8,1,2,2,3]
-Output: [4,0,1,1,3]
+        Input: nums = [8,1,2,2,3]
+        Output: [4,0,1,1,3]
 
-Psuedocode (brute force):
-Create a dictionary to map the current index to the number of occurrences found for that index
+        Psuedocode (brute force):
+        Create a dictionary to map the current index to the number of occurrences found for that index
+        
+        Loop over the numbers in input "nums"
+            
+            Create an inner loop to loop over all the numbers in the input
+                For each iteration, compare if the outer loop number is greater than the inner loop number.
+                    If it is, add 1 to the the number occurrences at that index        
+        Return the values of the dictionary
+        """
+        num_dict = {}
 
-Loop over the numbers in input "nums"
-
-    Create an inner loop to loop over all the numbers in the input
-        For each iteration, compare if the outer loop number is greater than the inner loop number.
-            If it is, add 1 to the the number occurrences at that index        
-Return the values of the dictionary
-"""
+        for idx, num in enumerate(nums):
+            num_dict[idx] = 0
+            for num_two in nums:
+                if num_two < num:
+                    num_dict[idx] += 1
+        print(num_dict)
+        return num_dict.values()
